58cfcc464dd1ed8a5b36ce941d4527da8627b896
Daniel Justice <danieljustice09@gmail.com>
2017-02-14 23:08:03 -0500

    images are being displayed on buttons and unit tests surrounding this have been added

--- src/main/java/Hnefatafl.java
+++ src/main/java/Hnefatafl.java
@@ -1,18 +1,40 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 
 public class Hnefatafl {
     JFrame _frame = new JFrame("Hnefatafl");
     JPanel _ttt = new JPanel();
     JPanel _newPanel = new JPanel();
-    JButton[][] _buttons = new JButton[11][11];
+    public JButton[][] _buttons = new JButton[11][11];
     int CLICKS = 0;
-
+    public ImageIcon defenseIcon;
+    public ImageIcon axeIcon;
+    public ImageIcon emptyImageIcon;
     public Hnefatafl() {
+        //pull in images for icons on the buttons
+    	try{
+            //ImageIcons are public so we can test them in unit tests
+    		defenseIcon = new ImageIcon(ImageIO.read(new File("src/First Shield.png")));
+            axeIcon = new ImageIcon(ImageIO.read(new File("src/First Axe.png")));
+      		emptyImageIcon =new ImageIcon(ImageIO.read(new File("src/empty.png")));
+
+            //give each icon a description so we can compare them later
+      		defenseIcon.setDescription("shield");
+      		axeIcon.setDescription("axe");
+      		emptyImageIcon.setDescription("empty");
+        }catch(Exception e){
+            //woops just in case we cant pull in a file
+            //Note: path for file we read must be relative to src/ folder
+        	System.out.println("WOOPS " + e);
+        }
     }
 
     public boolean drawBoard(){
+    	
         _frame = new JFrame("Hnefatafl");
         _frame.setSize(850, 850);
         _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,27 +73,27 @@ public class Hnefatafl {
             for (int j=0; j<11 ; j++) {
                 if((i == 0 && (j > 2 && j < 8)) || (i == 1 && (j == 5))){ 
                     //top black pieces
-                    _buttons[i][j] = new JButton("X");
+                    _buttons[i][j] = new JButton(axeIcon);
                 }
                 else if((j == 0 && (i > 2 && i < 8)) || (i == 5 && (j == 1))){ 
                     //left black pieces
-                    _buttons[i][j] = new JButton("X");
+                    _buttons[i][j] = new JButton(axeIcon);
                 }
                 else if((j == 10 && (i > 2 && i < 8)) || (i == 5 && (j == 9))){ 
                     //right black pieces
-                    _buttons[i][j] = new JButton("X");
+                    _buttons[i][j] = new JButton(axeIcon);
                 }
                 else if((i == 10 && (j > 2 && j < 8)) || (i == 9 && (j == 5))){ 
                     //bottom black pieces
-                    _buttons[i][j] = new JButton("X");
+                    _buttons[i][j] = new JButton(axeIcon);
                 }
                 else if((i == 3 && j == 5) || (i == 4 && (j > 3 && j <7)) || (i == 5 && (j > 2 && j <8)) || (i == 6 && (j > 3 && j <7)) || (i == 7 && j == 5)){
                     //center white pieces
-                    _buttons[i][j] = new JButton("O");
+                    _buttons[i][j] = new JButton(defenseIcon);
                 }
                 else{
                     // Make a new button in the array location with text "_"
-                    _buttons[i][j] = new JButton("_");
+                    _buttons[i][j] = new JButton(emptyImageIcon);
                 }
                 // Associate a new ButtonListener to the button (see below)
                 ActionListener buttonListener = new ButtonListener();
@@ -102,15 +124,19 @@ public class Hnefatafl {
 
         public void actionPerformed(ActionEvent e) {
             JButton source = (JButton) e.getSource();
-            String currentText = source.getText();
-            if (currentText.equals("_")) {
+            ImageIcon currentText = (ImageIcon)source.getIcon();
+            //compares the description of the ImageIcon on the button clicked to see if it is an
+            //empty space
+            if (currentText.getDescription().compareTo(emptyImageIcon.getDescription()) == 0) {
+                //if it is an empty space, set ImageIcon to axe or shield based off of # of clicks
                 if(CLICKS % 2 == 0)
-                    source.setText("X");
+                    source.setIcon(axeIcon);
                 else
-                    source.setText("0");
+                    source.setIcon(defenseIcon);
                 CLICKS++;
             } else {
-                source.setText("_");
+                //if not an empty button, make it an empty button
+                source.setIcon(emptyImageIcon);
             }
         }
     }
--- src/test/java/HnefataflTests.java
+++ src/test/java/HnefataflTests.java
@@ -1,13 +1,76 @@
 import org.junit.Test;
-
+import org.junit.Before;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 import static junit.framework.Assert.*;
 
 public class HnefataflTests {
-    private Hnefatafl game = new Hnefatafl();
+    private Hnefatafl game;
+
+    @Before
+    public void beforeTests(){
+    	game = new Hnefatafl();
+    }
+    @Test
+    public void testInstanceOfHnefatafl(){
+    	assertTrue(game != null);
+    	assertTrue(game instanceof Hnefatafl);
+    }
+
+    @Test
+    public void testImageIcons(){
+    	assertTrue(game.defenseIcon != null);
+    	assertTrue(game.axeIcon != null);
+    	assertTrue(game.emptyImageIcon != null);
+    }
+
 
     @Test
     public void testBoardDraws(){
         // Test that the board draws without error
         assertTrue(game.drawBoard());
+    }
+    @Test
+    public void testForEmptyButton(){
+    	//must set up the game on the drawnboard
+    	game.setupGame(); 
+    	JButton testButton = game._buttons[1][1];
+    	assertTrue(testButton != null);
+    	ImageIcon testImageIcon = (ImageIcon)testButton.getIcon();
+    	String testDescription = testImageIcon.getDescription();
+    	assertTrue(testDescription.equals("empty"));
+    }
+    @Test
+    //EXPECT THIS TO BECOME OBSOLETE AT ONE POINT
+    public void testClickCycle(){
+    	//this test will click an empty tile three (3) times.  The tile should turn
+    	//into an axe, then turn into an empty and then into a shield
+    	
+    	game.setupGame(); 
+    	JButton testButton = game._buttons[1][1];
+    	assertTrue(testButton != null);
+    	ImageIcon testImageIcon = (ImageIcon)testButton.getIcon();
+    	String testDescription = testImageIcon.getDescription();
+    	assertTrue(testDescription.equals("empty"));
+    	//first click changes it to an axe
+    	testButton.doClick();
+    	testImageIcon = (ImageIcon)testButton.getIcon();
+    	testDescription = testImageIcon.getDescription();
+    	assertTrue(testDescription.equals("axe"));
+    	//second click turns it empty
+    	testButton.doClick();
+    	testImageIcon = (ImageIcon)testButton.getIcon();
+    	testDescription = testImageIcon.getDescription();
+    	assertTrue(testDescription.equals("empty"));
+ 		//third click turns it into a shield
+    	testButton.doClick();
+    	testImageIcon = (ImageIcon)testButton.getIcon();
+    	testDescription = testImageIcon.getDescription();
+    	assertTrue(testDescription.equals("shield"));
+
     }
 }
