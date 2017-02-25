fa0ea8f7bdd5c550357187581fe473858f76b458
Timothy Kang <timothykang8@gmail.com>
2017-02-25 13:36:13 -0500

    fixed serializable conflict

--- src/main/java/Hnefatafl.java
+++ src/main/java/Hnefatafl.java
@@ -115,35 +115,37 @@ public class Hnefatafl {
 
         _newPanel = new JPanel();
         _newPanel.setLayout(new FlowLayout());
-        for(int i=0; i<11; i++){
-            for (int j=0; j<11 ; j++) {
-                if((i == 0 && (j > 2 && j < 8)) || (i == 1 && (j == 5))){
-                    //top black pieces
-                    _buttons[i][j] = new JButton(axeIcon);
-                }
-                else if((j == 0 && (i > 2 && i < 8)) || (i == 5 && (j == 1))){
-                    //left black pieces
-                    _buttons[i][j] = new JButton(axeIcon);
-                }
-                else if((j == 10 && (i > 2 && i < 8)) || (i == 5 && (j == 9))){
-                    //right black pieces
-                    _buttons[i][j] = new JButton(axeIcon);
-                }
-                else if((i == 10 && (j > 2 && j < 8)) || (i == 9 && (j == 5))){
-                    //bottom black pieces
-                    _buttons[i][j] = new JButton(axeIcon);
-                }
-                else if(i == 5 && j == 5){
-                    _buttons[i][j] = new JButton(kingIcon);
-                }
-                else if((i == 3 && j == 5) || (i == 4 && (j > 3 && j <7)) || (i == 5 && (j > 2 && j <8)) || (i == 6 && (j > 3 && j <7)) || (i == 7 && j == 5)){
-                    //center white pieces
-                    _buttons[i][j] = new JButton(defenseIcon);
-                }
-                else{
-                    // Make a new button in the array location with text "_"
-                    _buttons[i][j] = new JButton(emptyImageIcon);
+        for(int i=0; i<gameWidth; i++){
+            for (int j=0; j<gameHeight ; j++) {
+                if(_buttons[i][j] == null) {
+                    if((i == 0 && (j > 2 && j < 8)) || (i == 1 && (j == 5))){
+                        //top black pieces
+                        _buttons[i][j] = new JButton(axeIcon);
+                    }
+                    else if((j == 0 && (i > 2 && i < 8)) || (i == 5 && (j == 1))){
+                        //left black pieces
+                        _buttons[i][j] = new JButton(axeIcon);
+                    }
+                    else if((j == 10 && (i > 2 && i < 8)) || (i == 5 && (j == 9))){
+                        //right black pieces
+                        _buttons[i][j] = new JButton(axeIcon);
+                    }
+                    else if((i == 10 && (j > 2 && j < 8)) || (i == 9 && (j == 5))){
+                        //bottom black pieces
+                        _buttons[i][j] = new JButton(axeIcon);
+                    }
+                    else if(i == 5 && j == 5){
+                        _buttons[i][j] = new JButton(kingIcon);
+                    }
+                    else if((i == 3 && j == 5) || (i == 4 && (j > 3 && j <7)) || (i == 5 && (j > 2 && j <8)) || (i == 6 && (j > 3 && j <7)) || (i == 7 && j == 5)){
+                        //center white pieces
+                        _buttons[i][j] = new JButton(defenseIcon);
+                    }
+                    else{
+                        // Make a new button in the array location with text "_"
+                        _buttons[i][j] = new JButton(emptyImageIcon);
 
+                    }
                 }
                 // Associate a new ButtonListener to the button (see below)
                 ActionListener buttonListener = new ButtonListener();
