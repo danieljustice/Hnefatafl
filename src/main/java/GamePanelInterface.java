9bb7c0759bac20ddfd891dd3fbeb79294f4783d9
Speedy Turtle <danieljustice09@gmail.com>
2017-04-04 17:02:29 -0400

    removed gamepanelinterface.java because i dont think we need it

--- src/main/java/GamePanelInterface.java
+++
@@ -1,9 +0,0 @@
-
-
-public interface GamePanelInterface{
-
-	public boolean setupNewBoard();
-
-	public boolean setupExistingBoard(); //Some sort of data should be passed in but idk what?  Maybe an array of buttons?
-
-}
